6cf4de7e2a7e06af936db866b96fbebd435dd5e4
Artem Frolov <Artem.Frolov@kaspersky.com>
2022-03-20 15:10:53 +0300

    check

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# SECRET_KEY = "hr4=*i@t2-yby=4b37oc6-4%p#@i90a2*9g^3_d-)*bb#3*lw5"
+
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY", default="any_key")
 
